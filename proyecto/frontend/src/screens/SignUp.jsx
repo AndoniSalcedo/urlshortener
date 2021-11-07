f5bdb8caa912216458313b9f12f16da6f0d2ef3c
Tomenos18 <tomas.pel.ben@gmail.com>
2021-11-07 19:05:35 +0000

    go better frontend

---
+++ proyecto/frontend/src/screens/SignUp.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react'
+
+import AuthContext from '../contex'
+import axios from 'axios'
+
+import history from "../history"
+
+const SignUp = () => {
+    const [Email,setEmail] = useState("")
+    const [Password,setPassword] = useState("")
+    const [Name,setName] = useState("")
+    const { signIn } = useContext(AuthContext);
+    
+    const registerUser = async () => {
+        try{
+            const res = await axios.post("localhost:80//auth/register",{
+                name: Name,
+                email: Email,
+                password: Password
+            })
+            console.log(res.data)
+           history.push('/login')
+        }catch(err){
+            console.log(err)
+        }
+    }
+        return(
+            <section className="content">
+                <form onSubmit={()=>{}}>
+                    <h2 className="title">Regístrate</h2>
+                    <section className="input-section name">
+                        <section/> 
+                        <section>
+                            <input type="text" 
+                                placeholder="Name"
+                                className="input" 
+                                required
+                                value={Name}
+                                onChange={(v)=>{setName(v.target.value)}}
+                            />
+                        </section>
+                    </section>
+                    <section className="line"></section>
+                    <section className="input-section email">
+                        <section/> 
+                        <section>
+                            <input type="text" 
+                                placeholder="Email"
+                                className="input" 
+                                required
+                                value={Email}
+                                onChange={(v)=>{setEmail(v.target.value)}}
+                            />
+                        </section>
+                        
+                    </section>
+                    <section className="line"></section>
+                    <section className="input-section pass">
+                        <section/>
+           		        <section>
+                           <input type="password" 
+                                placeholder="Contraseña"
+                                className="input"
+                                required
+                                value={Password}
+                                onChange={(p)=>{setPassword(p.target.value)}}
+                            />
+            	        </section>
+                    </section>
+                    <section className="line"></section>
+                    <a className="login-component" href="/recovery">Forgot Password?</a>
+            	    <button type="button" className="btn" style={{backgroundImage: "linear-gradient(to right, #32be8f, #38d39f, #32be8f)", color: 'white'}} onClick={registerUser}>Register</button>
+                    <button type="button" className="btn" style={{backgroundImage: "linear-gradient(to right, #EA4C46, #F07470, #F1959B)", color: 'white'}} onClick={()=>{history.push('/signin')}}>SignIn</button>
+                </form>
+            </section>
+        );
+}
+
+export default SignUp;
