3b3e90a6ca697fd01123ae1fe07c241a7be4b997
Andoni Salcedo <andonisalcedo@gmail.com>
2021-11-09 18:40:20 +0100

    add findByUrl

--- proyecto/backend/src/main/kotlin/com/unizar/urlshorter/repositories/UrlRepository.kt
+++ proyecto/backend/src/main/kotlin/com/unizar/urlshorter/repositories/UrlRepository.kt
@@ -7,4 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository
 interface UrlRepository: MongoRepository<Url, String> {
     fun findOneByShorter(shorter: String): Url?
     fun findOneById(id: String): Url?
+    fun findOneByUrl(url: String): Url?
 }
