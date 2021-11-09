d8e6b646081601ec5248e45d1b2dae17e0071b71
unknown <pkmniako@gmail.com>
2021-11-09 17:44:53 +0100

    Backend responds to short to long petition, but can't find URL in database

--- proyecto/backend/src/main/kotlin/com/unizar/urlshorter/controllers/WSController.kt
+++ proyecto/backend/src/main/kotlin/com/unizar/urlshorter/controllers/WSController.kt
@@ -11,12 +11,19 @@ import java.lang.Thread
 
 import org.bson.types.ObjectId
 
+import com.unizar.urlshorter.repositories.UrlRepository
+import com.unizar.urlshorter.repositories.UserRepository
+import com.unizar.urlshorter.models.Url
+import com.unizar.urlshorter.models.User
+import com.unizar.urlshorter.controllers.UrlController
+import com.unizar.urlshorter.UrlshorterApplication
+
 /*
     Class that Controlls WebSocket Behaivour.
     Path to WebSocket initialized in UriShorterApplication.kt
 */
 @Component
-class WSController : TextWebSocketHandler() {
+class WSController(var urlRepository: UrlRepository, var userRepository: UserRepository) : TextWebSocketHandler() {
     
     //@Throws(Exception::class)
     override fun afterConnectionEstablished(session: WebSocketSession) {
@@ -32,17 +39,32 @@ class WSController : TextWebSocketHandler() {
     //@Throws(InterruptedException::class, IOException::class)
     public override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         //Test response
-		println("Incoming Message: '" + message.payload + "', waiting 10 seconds...");
 
         var shortURL = message.payload
+        //TODO: No se deberia rellenar la URL con localhost
+        shortURL = "http://localhost:3000/api/" + shortURL
 
-        //Retrieve long URL from short URL
+        //Find if the url exist
+        try {
+            var url = urlRepository.findOneByShorter(shortURL)
+            if(url == null) {
+                println("URL Error: '" + message.payload + "' Retrieved URL is null")
+                session.sendMessage(TextMessage("https://en.wikipedia.org/wiki/HTTP_404"))
+                return
+            }
 
-        var longURL = "http://www.google.com"
+            url.addClick()
 
-        Thread.sleep(10000)
+            var longURL = url.url
 
-        println("Sending Response...");
-        session.sendMessage(TextMessage(longURL))
+            println("Incoming Request: '" + message.payload + ". URL found: " + url.url + ". Waiting 10 seconds...");
+            Thread.sleep(10000) //Wait 10 seconds
+        
+            println("Sending Response...");
+            session.sendMessage(TextMessage(longURL))
+        } catch(e: Exception) {
+            println("Invalid Request: Short URL '" + message.payload + "' doesn't exist")
+            session.sendMessage(TextMessage("https://en.wikipedia.org/wiki/HTTP_404"))
+        }
     }
 }
