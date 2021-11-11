a73e356e6fdc7dfa2fa5beee3a48565dd1d5c710
Tomenos18 <tomas.pel.ben@gmail.com>
2021-11-11 15:38:55 +0000

    varios cambios sobre la apariencia

--- proyecto/frontend/src/screens/UrlPage.jsx
+++ proyecto/frontend/src/screens/UrlPage.jsx
@@ -5,81 +5,86 @@ import axios from 'axios'
 import history from "../history"
 
 const UrlPage = () => {
-    const [urlToShort,seturlToShort] = useState("")
-    
-    const [urlShorted, setUrlShorted]= useState("")
+    const [urlToShort, seturlToShort] = useState("")
+
+    const [urlShorted, setUrlShorted] = useState("")
 
     const [qr, setQr] = useState("")
 
     const shortUrl = async () => {
-        if(urlToShort === ""){
+        if (urlToShort === "") {
 
-        }else{
-            try{
-                const res = await axios.post("http://localhost:8080/api/shorter",{
+        } else {
+            try {
+                const res = await axios.post("http://localhost:8080/api/shorter", {
                     url: urlToShort,
                 })
-                setUrlShorted("http://localhost:3000/s/"+res.data.url)
+                setUrlShorted("http://localhost:3000/s/" + res.data.url)
                 console.log(res.data)
                 console.log(res.data.url)
                 console.log(urlShorted)
-            }catch(err){
+            } catch (err) {
                 console.log(err)
             }
         }
     }
 
     const getQr = async () => {
-        if(urlShorted === ""){
+        if (urlShorted === "") {
 
-        }else{
-                try{
-                    const res = await axios.post("http://localhost:8080/api/qr",{
-                        url: urlToShort,
-                    })
-                    setQr(res.data.qr)
+        } else {
+            try {
+                const res = await axios.post("http://localhost:8080/api/qr", {
+                    url: urlToShort,
+                })
+                setQr(res.data.qr)
 
-                }catch(err){
-                    console.log(err)
-                }
+            } catch (err) {
+                console.log(err)
             }
         }
+    }
 
 
 
-            return(
-                <section className="content">
-                    <form onSubmit={()=>{}}>
-                        <h1 className="title">Urlshorter</h1>
-                        <section className="input-section email">
-                            <section/>
-                            <section>
-                                <input type="text"
-                                    placeholder="url to short"
-                                    className="input"
-                                    required
-                                    value={urlToShort}
-                                    onChange={(v)=>{seturlToShort(v.target.value)}}
-                                />
-                            </section>
+    return (
+        <section className="content">
+            <form onSubmit={() => { }}>
+                <h1 className="title">Urlshorter</h1>
+                <section className="input-section email">
+                    <section />
+                    <section>
+                        <input type="text"
+                            placeholder="url to short"
+                            className="input"
+                            required
+                            value={urlToShort}
+                            onChange={(v) => { seturlToShort(v.target.value) }}
+                        />
+                    </section>
+                    {urlShorted !== "" ?
+                        <>
                             <p>Url recortada: {urlShorted}</p>
-
-                            <button type="button" className="btn" onClick={shortUrl}>Recortar</button>
-                            <button type="button" className="btn" onClick={()=>{history.push('/signin')}}>SignIn</button>
-                            <button type="button" className="btn" onClick={()=>{history.push('/signup')}}>Registrarse</button>
-                            <button type="button" className="btn" onClick={getQr}>Generar QR</button>
-                            { qr !== "" ?
-                                <>
-                                    <p>Código QR: </p>
-                                    <img src={"data:image/png;base64," +  qr} />
-                                </>
-                                :
-                                <p/>
-                            }
-                        </section>
-                    </form>
+                        </>
+                        :
+                        <p />
+                    }
+                    <button type="button" className="btn" onClick={shortUrl}>Recortar</button>
+                    <button type="button" className="btn" onClick={() => { history.push('/signin') }}>SignIn</button>
+                    <button type="button" className="btn" onClick={() => { history.push('/signup') }}>Registrarse</button>
+                    <button type="button" className="btn" onClick={getQr}>Generar QR</button>
+                    {qr !== "" ?
+                        <>
+                            <p>Código QR: </p>
+                            <img src={"data:image/png;base64," + qr} />
+                        </>
+                        :
+                        <p />
+                    }
                 </section>
-            );
+            </form>
+        </section>
+    );
 }
 
 export default UrlPage;
--- proyecto/frontend/src/screens/UrlUserPage.jsx
+++ proyecto/frontend/src/screens/UrlUserPage.jsx
@@ -5,48 +5,82 @@ import axios from 'axios'
 import history from "../history"
 
 const UrlUserPage = () => {
-    const [urlToShort,seturlToShort] = useState("")
-    
-    const [urlShorted, setUrlShorted]= useState("Not url shorted yet")
+    const [urlToShort, seturlToShort] = useState("")
+
+    const [urlShorted, setUrlShorted] = useState("")
+
+    const [qr, setQr] = useState("")
 
     const token = history.location.state?.token
-    
+
     const shortUrl = async () => {
-        try{
-            console.log(token)
-            const res = await axios.post("http://localhost:8080/api/user/shorter",
-                {url: urlToShort},
-                {headers: {"accessToken" : token}})
-            setUrlShorted("http://localhost:3000/s/"+res.data.url)
-            console.log(res.data)
-            console.log(res.data.url)
-            console.log(urlShorted)
-        }catch(err){
-            console.log(err)
+        if (urlToShort !== "") {
+            try {
+                console.log(token)
+                const res = await axios.post("http://localhost:8080/api/user/shorter",
+                    { url: urlToShort },
+                    { headers: { "accessToken": token } })
+                setUrlShorted("http://localhost:3000/s/" + res.data.url)
+                console.log(res.data)
+                console.log(res.data.url)
+                console.log(urlShorted)
+            } catch (err) {
+                console.log(err)
+            }
         }
     }
-        return(
-            <section className="content">
-                <form onSubmit={()=>{}}>
-                    <h2 className="title">Url to short</h2>
-                    <section className="input-section email">
-                        <section/> 
-                        <section>
-                            <input type="text" 
-                                placeholder="url to short"
-                                className="input" 
-                                required
-                                value={urlToShort}
-                                onChange={(v)=>{seturlToShort(v.target.value)}}
-                            />
-                        </section>
-                        <p>Url recortada: {urlShorted}</p>
-                        <button type="button" className="btn" onClick={shortUrl}>Recortar</button>
-                        <button type="button" className="logout" onClick={()=>{history.push('/signout')}}>Log-out</button>
+    const getQr = async () => {
+        if (urlShorted === "") {
+
+        } else {
+            try {
+                const res = await axios.post("http://localhost:8080/api/qr", {
+                    url: urlToShort,
+                })
+                setQr(res.data.qr)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }
+
+    return (
+        <section className="content">
+            <form onSubmit={() => { }}>
+                <h2 className="title">Url to short</h2>
+                <section className="input-section email">
+                    <section />
+                    <section>
+                        <input type="text"
+                            placeholder="url to short"
+                            className="input"
+                            required
+                            value={urlToShort}
+                            onChange={(v) => { seturlToShort(v.target.value) }}
+                        />
                     </section>
-                </form>
-            </section>
-        );
+                    {urlShorted !== "" ?
+                        <>
+                        <p>Url recortada: {urlShorted}</p>
+                        </>
+                        :
+                        <p/>
+                    }
+                    <button type="button" className="btn" onClick={shortUrl}>Recortar</button>
+                    <button type="button" className="btn" onClick={getQr}>Generar QR</button>
+                    <button type="button" className="logout" onClick={() => { history.push('/signout') }}>Log-out</button>
+                    {qr !== "" ?
+                        <>
+                            <p>Código QR: </p>
+                            <img src={"data:image/png;base64," + qr} />
+                        </>
+                        :
+                        <p />
+                    }
+                </section>
+            </form>
+        </section>
+    );
 }
 
 export default UrlUserPage;
